babe13deb197c5fcddfd1a4849fc08ac4a3149f6
ayesha-the-coder <ayeshafarhat4me@gmail.com>
2026-03-03 11:05:27 -0800

    Add new files

---
+++ JSLec-11/JS_Lecture-11.js
@@ -0,0 +1,78 @@
+//Simple Array Example
+
+// let myArray = [,10,20,30,40,50,60,70];
+// console.log(myArray[3]);
+
+//Array Method toString() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// console.log(myArray);
+// let s = myArray.toString();
+// console.log(s);
+
+//Array Method join() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// let j = myArray.join("_");
+// console.log(j);
+
+
+//Array Method pop() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+
+
+
+//Array Method push() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+//  myArray.push(100);
+// console.log(myArray);
+
+//Array Method shift() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// let myshift =  myArray.shift();
+// console.log(myshift);
+
+//Array Method unshift() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// myArray.unshift(500);
+// console.log(myArray);
+
+
+//Array Method concat() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// let arrayAgain = [210,220,230,240,250,260,270];
+// let array2 = [310,320,330,340,350, 260,370];
+// let c = myArray.concat(arrayAgain, array2);
+// console.log(c);
+
+
+//Array Method delete() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// delete myArray[4];
+// console.log(myArray);
+// console.log(myArray.length);
+
+
+//Array Method splice() Example
+
+// let myArray = [10,20,30,40,50,60,70];
+// myArray.splice(2,1,500,600,700);
+// console.log(myArray);
+
+
+//Array Method slice() Example
+
+let myArray = [10,20,30,40,50,60,70];
+let newarray = myArray.slice(1,3);
+console.log(newarray);
+
+
